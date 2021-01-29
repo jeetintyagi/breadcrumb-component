8743e4fc9e4e6a6bfc5291663e2df69b0e92ddf1
Jitin Tyagi <jt9999709701@gmail.com>
2021-01-29 07:34:12 +0000

    Initial commit

---
+++ src/component/CollapseBreadcrumb.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MdMoreHoriz } from 'react-icons/md'
+
+const CollapseBreadcrumb = (props) => (
+  <li className='breadcrumb-collapser' {...props}>
+    <MdMoreHoriz />
+  </li>
+)
+
+export default CollapseBreadcrumb
